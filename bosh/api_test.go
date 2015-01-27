99bef80e03ee679587c0031690d74a8a4240433d
datianshi <dsz0111@gmail.com>
2015-01-27 13:17:49 -0500

    [#86804234] Refactor the bosh execution.

--- bosh/api_test.go
+++ bosh/api_test.go
@@ -8,26 +8,20 @@ import (
 
 var _ = Describe("API", func() {
 	var (
-		api         *API
 		pathParams  map[string]string = map[string]string{"deployment": "cf-1", "job": "cloudcontroller", "index": "1"}
 		queryParams map[string]string = map[string]string{"state": "started"}
 		ip          string            = "10.10.10.10"
 		port        int               = 25555
+		path        string            = "deployments/{deployment}/{job}/{index}"
 	)
 	Describe("Generate Director url", func() {
-		BeforeEach(func() {
-			api = &API{
-				Path: "deployments/{deployment}/{job}/{index}",
-			}
-
-		})
 		Context("With valid configureation", func() {
 			It("should return nil error", func() {
-				_, err := api.GetUrl(ip, port, pathParams, queryParams)
+				_, err := ParseUrl(ip, port, path, pathParams, queryParams)
 				Expect(err).Should(BeNil())
 			})
 			It("should compose a valid url", func() {
-				url, _ := api.GetUrl(ip, port, pathParams, queryParams)
+				url, _ := ParseUrl(ip, port, path, pathParams, queryParams)
 				Expect(url).To(Equal("https://10.10.10.10:25555/deployments/cf-1/cloudcontroller/1?state=started"))
 			})
 		})
