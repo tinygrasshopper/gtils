c2ada3341a777624c4bb870ab1ac143824ec9ea6
John Calabrese <jcalabrese@pivotal.io>
2015-02-24 17:26:11 -0500

    [#88869290] first pass bugfix
    needs tests, but this is to implement a fix for
    multipart uploads....

--- http/http_suite_test.go
+++ http/http_suite_test.go
@@ -1,6 +1,8 @@
 package http_test
 
 import (
+	"net/http"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -10,4 +12,15 @@ import (
 func TestHttp(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Http Suite")
+}
+
+type transportClientInterface interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+type mockClientTransport struct{}
+
+func (s *mockClientTransport) Do(req *http.Request) (res *http.Response, err error) {
+
+	return
 }
--- http/multipart_test.go
+++ http/multipart_test.go
@@ -1,6 +1,7 @@
 package http_test
 
 import (
+	"net/http"
 	"os"
 
 	. "github.com/onsi/ginkgo"
@@ -9,24 +10,50 @@ import (
 )
 
 var _ = Describe("Multipart", func() {
-	var (
-		multipartConstructor MultiPartBodyFunc = MultiPartBody
-	)
-	Context("Construct the multipart body successfully", func() {
-		It("Should return nil error", func() {
-			fileRef, _ := os.Open("fixtures/installation.json")
-			body, err := multipartConstructor("installation[file]", "installation.json", fileRef, nil)
-			Ω(err).Should(BeNil())
-			Ω(body).ShouldNot(BeNil())
+	Describe("MultiPartBody", func() {
+		var (
+			multipartConstructor MultiPartBodyFunc = MultiPartBody
+		)
+		Context("Construct the multipart body successfully", func() {
+			It("Should return nil error", func() {
+				fileRef, _ := os.Open("fixtures/installation.json")
+				body, contentType, err := multipartConstructor("installation[file]", "installation.json", fileRef, nil)
+				Ω(err).Should(BeNil())
+				Ω(body).ShouldNot(BeNil())
+				Ω(contentType).ShouldNot(BeNil())
+			})
+		})
+
+		Context("Construct the multipart body failed", func() {
+			It("Should return error when file is missing", func() {
+				fileRef, _ := os.Open("fixtures/installa.json")
+				_, _, err := multipartConstructor("installation[file]", "installation.json", fileRef, nil)
+				Ω(err).ShouldNot(BeNil())
+			})
 		})
 	})
 
-	Context("Construct the multipart body failed", func() {
-		It("Should return error when file is missing", func() {
-			fileRef, _ := os.Open("fixtures/installa.json")
-			_, err := multipartConstructor("installation[file]", "installation.json", fileRef, nil)
-			Ω(err).ShouldNot(BeNil())
+	Describe("MultiPartUpload", func() {
+		var transportClientFunc func() (client interface {
+			Do(*http.Request) (*http.Response, error)
+		})
+
+		BeforeEach(func() {
+			transportClientFunc = NewTransportClient
+			NewTransportClient = func() (client interface {
+				Do(*http.Request) (*http.Response, error)
+			}) {
+				client = new(mockClientTransport)
+				return
+			}
+		})
+
+		AfterEach(func() {
+			NewTransportClient = transportClientFunc
+		})
+
+		It("should do something", func() {
+			Ω("hi").Should(Equal("hi"))
 		})
 	})
-
 })
