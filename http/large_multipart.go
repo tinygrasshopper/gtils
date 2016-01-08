4ca9dff29446be3bfef32458956ad88b55cc3234
Mark Alston <malston@pivotal.io>
2016-01-08 13:38:12 -0700

    Move file size calculation into test

--- http/large_multipart.go
+++ http/large_multipart.go
@@ -4,15 +4,14 @@ import (
 	"crypto/tls"
 	"io"
 	"net/http"
-	"os"
 
 	"github.com/technoweenie/multipartstreamer"
 )
 
-var LargeMultiPartUpload = func(conn ConnAuth, paramName, filename string, fileRef io.Reader, params map[string]string) (res *http.Response, err error) {
+var LargeMultiPartUpload = func(conn ConnAuth, paramName, filename string, fileSize int64, fileRef io.Reader, params map[string]string) (res *http.Response, err error) {
 	var req *http.Request
 	ms := multipartstreamer.New()
-	ms.WriteReader(paramName, filename, getFileSize(filename), fileRef)
+	ms.WriteReader(paramName, filename, fileSize, fileRef)
 	if req, err = http.NewRequest("POST", conn.Url, nil); err == nil {
 		if conn.Username != "" && conn.Password != "" {
 			req.SetBasicAuth(conn.Username, conn.Password)
@@ -23,24 +22,6 @@ var LargeMultiPartUpload = func(conn ConnAuth, paramName, filename string, fileR
 		}
 		client := &http.Client{Transport: tr}
 		res, err = client.Do(req)
-	}
-	return
-}
-
-func getFileSize(filename string) (fileSize int64) {
-	var (
-		fileInfo os.FileInfo
-		err      error
-		file     *os.File
-	)
-
-	if file, err = os.Open(filename); err == nil {
-		fileInfo, err = file.Stat()
-		fileSize = fileInfo.Size()
-	}
-
-	if err != nil {
-		fileSize = -1
 	}
 	return
 }
