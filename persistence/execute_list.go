02f6e3fc7007ce437a04c201c60025dcda26fe04
Caleb Washburn <cwashburn@pivotal.io>
2016-04-28 09:35:00 -0700

    [#118571577] ensuring restore output is set to stdout vs discarded

--- persistence/execute_list.go
+++ persistence/execute_list.go
@@ -1,7 +1,7 @@
 package persistence
 
 import (
-	"io/ioutil"
+	"os"
 
 	"github.com/pivotalservices/gtils/command"
 	"github.com/xchapter7x/lo"
@@ -11,7 +11,7 @@ func executeList(callList []string, caller command.Executer) (err error) {
 	for _, callstring := range callList {
 		lo.G.Debug(callstring)
 
-		if err = caller.Execute(ioutil.Discard, callstring); err != nil {
+		if err = caller.Execute(os.Stdout, callstring); err != nil {
 			lo.G.Error(err.Error())
 			break
 		}
