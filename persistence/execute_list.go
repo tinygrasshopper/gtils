1efef62457c8a37651589aa5c4cfe284098410b7
Jatin Naik <jnaik@pivotal.io>
2016-08-08 17:00:39 +0100

    print only the first part of execute list, as the later bits will have secrets
    Signed-off-by: Henry Stanley <hstanley@pivotal.io>

--- persistence/execute_list.go
+++ persistence/execute_list.go
@@ -2,6 +2,7 @@ package persistence
 
 import (
 	"os"
+	"strings"
 
 	"github.com/pivotalservices/gtils/command"
 	"github.com/xchapter7x/lo"
@@ -9,7 +10,7 @@ import (
 
 func executeList(callList []string, caller command.Executer) (err error) {
 	for _, callstring := range callList {
-		lo.G.Debug(callstring)
+		lo.G.Debug("executing: " + strings.Split(callstring, " ")[0])
 
 		if err = caller.Execute(os.Stdout, callstring); err != nil {
 			lo.G.Error(err.Error())
